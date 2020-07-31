e37c64960a48a29cb08edcc1e5f34b8b435c431b
Collin <collinhodgs@gmail.com>
2020-07-30 18:18:12 -0600

    got 2 more tests to pass

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -6,6 +6,14 @@ class Employee {
     this.id = id;
     this.email = email;
   }
+
+  getName() {
+    return this.name;
+  }
+
+  getId() {
+    return this.id;
+  }
 }
 
 module.exports = Employee;
