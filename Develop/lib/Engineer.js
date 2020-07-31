7a0913cf7e937536f538c3f8c9a67c87b00ad508
Collin <collinhodgs@gmail.com>
2020-07-30 18:37:26 -0600

    Exporting employee.js information to engineer.js

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,11 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js");
+
+class Engineer extends Employee {
+  constructor(name, id, email, gitHubUser) {
+    super(name, id, email);
+    this.github = gitHubUser;
+  }
+}
+
+module.exports = Engineer;
